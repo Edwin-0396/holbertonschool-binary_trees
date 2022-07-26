9687ea6776dcb83934c100d7aae3dcada5754f3b
Edwin-9603 <egc4611@gmail.com>
2022-07-26 11:52:42 -0400

    function that checks if a node is a leaf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,16 +1,31 @@
 #include "binary_trees.h"
-
+#include <stdlib.h>
 /**
- * binary_tree_insert_left -  function that inserts a node as the left-child of another node
+ * @brief
  *
- * @parent: pointer to the node to insert the left-child in
- * @value:  value to store in the new node
- *
- * Return: a pointer to the new node, or NULL on failure
  */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new = NULL;
+    binary_tree_t *temp = NULL;   
 
- binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
- {
+    new = malloc(sizeof(binary_tree_t));
+    if (!new || !parent)
+        return (NULL);
+    
 
+    new->n = value;
+    new->parent = parent;
+    new->left = new->right = NULL;
+    if (!parent->left)
+        parent->left = new;
 
- }
+    else
+    {
+        temp = parent->left;
+        parent->left = new;
+        new->left = temp;
+        temp->parent = new;
+    }
+    return (new);
+}
