bfe3e55b5084367c6ed7735d09428bdc91748908
Edwin-9603 <egc4611@gmail.com>
2022-07-28 19:32:09 -0400

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete: function that deletes an entire binary tree
+ * binary_tree_preorder - function that deletes an entire binary tree
+ * @func: pointer to a function to call for each node. The value in
+ * the node must be passed as a parameter to this function.
  * @tree: binary tree to delete
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(!tree || !func)
+	if (!tree || !func)
 		return;
 
 	func(tree->n);
