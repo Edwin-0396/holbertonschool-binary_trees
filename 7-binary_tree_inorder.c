f2e12541d08951d1ce77259ed16b9267dc890088
Edwin-9603 <egc4611@gmail.com>
2022-07-28 19:38:54 -0400

    function that goes through a binary tree using in-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete: function that deletes an entire binary tree
+ * binary_tree_inorder - function that goes through a
+ * binary tree using in-order traversal
  * @tree: binary tree to delete
+ * @func:  pointer to a function to call for each node.
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(!tree || !func)
+	if (!tree || !func)
 		return;
 
 	binary_tree_inorder(tree->left, func);
