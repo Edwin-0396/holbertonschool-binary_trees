3a6e427f24a13d8d141ec75ccb237360d726f5e4
Edwin-9603 <egc4611@gmail.com>
2022-07-28 19:44:34 -0400

    function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,23 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete: function that deletes an entire binary tree
+ * binary_tree_height - function that deletes an entire binary tree
  * @tree: binary tree to delete
+ * Return: tree is NULL, your function must return 0
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l = 0, height_r = 0;
 
-	if(!tree)
+	if (!tree)
 		return (0);
 
-	if(!tree->left && !tree->right)
+	if (!tree->left && !tree->right)
 		return (0);
 
 	height_l = binary_tree_height(tree->left);
 	height_r = binary_tree_height(tree->right);
-	if(height_l >= height_r)
+	if (height_l >= height_r)
 		return (height_l + 1);
 	return (height_r + 1);
 }
