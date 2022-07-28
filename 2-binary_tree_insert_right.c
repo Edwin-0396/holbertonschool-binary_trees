049f9b771791afd5f6df8baaa6a1c0432c8cd7f8
Edwin-9603 <egc4611@gmail.com>
2022-07-28 19:14:38 -0400

    new 2 task - binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_right = NULL;
 	binary_tree_t *temp = NULL;
 
+	if (!parent)
+		return (NULL);
+
 	new_right = malloc(sizeof(binary_tree_t));
 	if (!new_right || !parent)
 		return (NULL);
