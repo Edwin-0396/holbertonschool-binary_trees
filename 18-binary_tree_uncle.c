40c54a9a7ec6bf95271668b88308aa07a2cbc339
Edwin-9603 <egc4611@gmail.com>
2022-07-28 21:55:06 -0400

    function that finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,8 +3,8 @@
 #include "binary_trees.h"
 
 /**
- * aux_aux_binary_tree_height - measures the height from a noheighte in a binary tree
- * @tree: pointer to the noheighte to measure the height
+ * binary_tree_sibling - measures the height from a noheighte in a binary tree
+ * @node: pointer to the noheighte to measure the height
  * Return: height of the pointer, or 0 if tree is NULL
  */
 
@@ -20,17 +20,15 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 }
 
 /**
- * aux_aux_binary_tree_height - measures the height from a noheighte in a binary tree
- * @tree: pointer to the noheighte to measure the height
+ * binary_tree_uncle - measures the height from a noheighte in a binary tree
+ * @node: pointer to the noheighte to measure the height
  * Return: height of the pointer, or 0 if tree is NULL
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
 	if (!node)
 		return (NULL);
 	
 	return (binary_tree_sibling(node->parent));
-
 }
