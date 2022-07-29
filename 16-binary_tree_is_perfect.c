d1b2d2b75371860e28ec1cc349585ddb896ac9c5
Edwin-9603 <egc4611@gmail.com>
2022-07-28 21:45:49 -0400

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,9 +3,10 @@
 #include "binary_trees.h"
 
 /**
- * aux_aux_binary_tree_height - measures the height from a noheighte in a binary tree
- * @tree: pointer to the noheighte to measure the height
- * Return: height of the pointer, or 0 if tree is NULL
+ * aux_binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
  */
 
 int aux_binary_tree_height(const binary_tree_t *tree)
@@ -22,29 +23,35 @@ int aux_binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
-**/
-
-/* This function tests if a binary tree is perfect
-   or not. It basically checks for two things :
-   1) All leaves are at same level
-   2) All internal noheightes have two chilheightren */
+ * isPerfectRec - checks if a binary tree is perfect
+ * @root: pointer to the root node of the tree to check
+ * @height: gets the height of the tree
+ * @level: the level of the actual tree
+ *
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
 int isPerfectRec(const binary_tree_t *root, int height, int level)
 {
-    if (root == NULL)
-        return (0);
- 
-    if (root->left == NULL && root->right == NULL)
-        return (height == level ? 1 : 0);
- 
-    if (root->left == NULL || root->right == NULL)
-        return (0);
+	if (root == NULL)
+		return (0);
 
-    return isPerfectRec(root->left, height, level+1) *
-           isPerfectRec(root->right, height, level+1);
+	if (root->left == NULL && root->right == NULL)
+		return (height == level ? 1 : 0);
+
+	if (root->left == NULL || root->right == NULL)
+		return (0);
+
+	return (isPerfectRec(root->left, height, level + 1) *
+			isPerfectRec(root->right, height, level + 1));
 }
 
 
-
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	return (isPerfectRec(tree, aux_binary_tree_height(tree), 0));
